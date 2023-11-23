7bedf343969c573dc1608bd1410498e9958a19b4
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2023-11-23 13:39:57 -0400

    Validar mensajes de error en usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,58 +4,64 @@ const UsuarioController = require('../controllers/Usuario_Controller');
 const { checkLogin, checkAdmin, checkRoot, checkDatetime } = require('../auth/auth');
 
 /* GET user. */
-router.get('/', checkLogin,function(req, res, next){
-  UsuarioController.ver_usuarios().then((resultados)=>{
+router.get('/', checkLogin, function (req, res, next) {
+  UsuarioController.ver_usuarios().then((resultados) => {
     res.json(resultados);
-  }).catch((error)=>{
-        res.status(error.codigo).send(error)
+  }).catch((error) => {
+    if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+    else { res.status(500).send(error) }
   })
 });
 
-router.get('/:index', function(req, res, next){
-  UsuarioController.encontrar_usuario(req.params.index).then((resultado)=>{
+router.get('/:index', function (req, res, next) {
+  UsuarioController.encontrar_usuario(req.params.index).then((resultado) => {
     res.json(resultado);
-  }).catch((error)=>{
-    res.status(error.codigo).send(error)
+  }).catch((error) => {
+    if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+    else { res.status(500).send(error) }
   })
 });
 
 /* POST new user. */
-router.post('/crear', function(req, res, next){
-  UsuarioController.registrar_usuario(req.body).then((resultado)=>{ //token
-      //res.send(token)
-      console.log(req.body)
-      res.send(resultado)
-    }).catch((error)=>{
-      res.status(error).send(error)
-    })
+router.post('/crear', function (req, res, next) {
+  UsuarioController.registrar_usuario(req.body).then((resultado) => { //token
+    //res.send(token)
+    console.log(req.body)
+    res.send(resultado)
+  }).catch((error) => {
+    if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+    else { res.status(500).send(error) }
+  })
 });
 
-router.post('/login', function(req, res, next) {
-  UsuarioController.login(req.body).then((token)=>{ 
-      res.send(token)
-    }).catch((error)=>{
-      res.status(error).send(error)
-    })
+router.post('/login', function (req, res, next) {
+  UsuarioController.login(req.body).then((token) => {
+    res.send(token)
+  }).catch((error) => {
+    if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+    else { res.status(500).send(error) }
+  })
 });
 
 /* PUT user. */
-router.put('/:index',checkAdmin, function(req, res, next) {
-  UsuarioController.modificar_usuario(req.params.index, req.body).then((token)=>{ 
+router.put('/:index', checkAdmin, function (req, res, next) {
+  UsuarioController.modificar_usuario(req.params.index, req.body).then((token) => {
     res.send(token)
-  }).catch((error)=>{
-    res.status(error.codigo).send(error)
+  }).catch((error) => {
+    if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+    else { res.status(500).send(error) }
   })
 });
 
 /* DELETE user. */
-router.delete('/:index',checkAdmin, function(req, res, next){
-  UsuarioController.borrar_usuario(req.params.index).then((resultado)=>{ 
+router.delete('/:index', checkAdmin, function (req, res, next) {
+  UsuarioController.borrar_usuario(req.params.index).then((resultado) => {
     res.send(resultado)
-  }).catch((error)=>{
-    res.status(error.codigo).send(error)
+  }).catch((error) => {
+    if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+    else { res.status(500).send(error) }
   })
-}); 
+});
 
 /* PATCH user. */
 router.patch('/user_manager', checkRoot, function (req, res, next) {
@@ -66,7 +72,8 @@ router.patch('/user_manager', checkRoot, function (req, res, next) {
     })
     .catch((error) => {
       //EDITAR COD ERROR
-      res.status(error).send(error);
+      if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+      else { res.status(500).send(error) }
     })
 });
 
@@ -74,7 +81,7 @@ router.patch('/user_manager', checkRoot, function (req, res, next) {
 
 
 /*Views */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
